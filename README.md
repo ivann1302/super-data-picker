589abfb7599201fb7f0eb712f276ba94cdcb76ca
ivan <ivann97n@gmail.com>
2025-09-26 16:20:29 +0300

    build: create project infrostructure

--- README.md
+++
@@ -1,9 +0,0 @@
-# Super Data Picker
-
-Проект инициализирован. Стартовый шаблон Vite/React удален.
-
-Дальнейшая разработка:
-- Запуск в режиме разработки: npm run dev
-- Сборка: npm run build
-- Предпросмотр сборки: npm run preview
-- Линтинг: npm run lint
--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,6 +15,8 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      // Disable rules that might conflict with Prettier
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
